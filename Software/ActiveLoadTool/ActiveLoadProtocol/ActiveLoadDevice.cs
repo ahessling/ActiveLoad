30c395e386b99cb4350c52ca8c8e7ad9b42fa046
Andre Heßling <andre@andrehessling.de>
2016-03-13 18:15:15 +0100

    feat: added getter functions to read system state of device feat: added setter functions to set system state of device feat: added function to find all active load devices

--- Software/ActiveLoadTool/ActiveLoadProtocol/ActiveLoadDevice.cs
+++ Software/ActiveLoadTool/ActiveLoadProtocol/ActiveLoadDevice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,33 @@ namespace ActiveLoadProtocol
 {
     public class ActiveLoadDevice
     {
+        #region Exceptions
+        [Serializable]
+        public class DeviceNotFoundException : Exception
+        {
+            public DeviceNotFoundException() { }
+            public DeviceNotFoundException(string message) : base(message) { }
+            public DeviceNotFoundException(string message, Exception inner) : base(message, inner) { }
+            protected DeviceNotFoundException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context)
+            { }
+        }
+        #endregion
+
+        #region Private members
         SerialPortBuffered serialPort;
         SimpleSCPIProtocol scpiProtocol;
+        #endregion
 
+        #region Properties
         public string PortName
         {
             get; private set;
         }
+        #endregion
 
+        #region Constructors
         public ActiveLoadDevice()
         {
         }
@@ -29,44 +50,188 @@ namespace ActiveLoadProtocol
         {
             this.serialPort = serialPort;
         }
+        #endregion
 
-        string FindDevice()
+        #region Device open/close handling
+        public async Task<string[]> FindDevicesAsync()
         {
-            throw new NotImplementedException();
+            string[] portNames = SerialPort.GetPortNames();
+            List<string> listPortNames = new List<string>();
+
+            foreach (string portName in portNames)
+            {
+                try
+                {
+                    Open(portName);
+
+                    // Probe device using SCPI request *IDN?
+                    string response = await GetIdnAsync();
+
+                    if (response.Contains("Active Load"))
+                    {
+                        Debug.WriteLine("Found device at: " + portName);
+
+                        Close();
+
+                        listPortNames.Add(portName);
+                    }
+
+                    Close();
+                }
+                catch (Exception)
+                {
+                    // Continue with next device
+                }
+            }
+
+            return listPortNames.ToArray();
         }
 
-        public void Open()
+        public async Task Open()
         {
-            if (PortName == "")
+            if (PortName == null)
             {
-                // Try to find device and open it
-                PortName = FindDevice();
+                // Try to find first device and open it
+                string[] portNames = await FindDevicesAsync();
 
-                throw new NotImplementedException();
+                if (portNames.Length == 0)
+                {
+                    // Nothing found
+                    throw new DeviceNotFoundException("No device could be found.");
+                }
+
+                PortName = portNames[0];
             }
 
-            if (PortName != "")
-            {
-                // Try to open specified device
-                serialPort = new SerialPortBuffered(PortName);
-                serialPort.Open();
+            Open(PortName);
+        }
 
-                scpiProtocol = new SimpleSCPIProtocol(serialPort);
-            }
+        void Open(string portName)
+        {
+            // Try to open specified device
+            serialPort = new SerialPortBuffered(portName);
+            serialPort.Open();
+
+            scpiProtocol = new SimpleSCPIProtocol(serialPort);
         }
 
         public void Close()
         {
             serialPort.Close();
         }
+        #endregion
 
-        public async Task<string> ReadActualCurrent()
+        #region Functions: Get system state
+        public async Task<double> GetActualCurrentAsync()
         {
-            string response = await scpiProtocol.Request("MEAS:CURR");
+            string response = await scpiProtocol.RequestAsync("MEAS:CURR");
 
-            Console.WriteLine("Read actual current: " + response);
+            if (response.EndsWith(" mA"))
+            {
+                try
+                {
+                    return double.Parse(response.Split(' ')[0]) / 1000;
+                }
+                catch (Exception e)
+                {
+                    throw new UnexpectedResponseException("Could not parse: " + response, e);
+                }
+            }
+            else
+            {
+                throw new UnexpectedResponseException("Unexpected response: " + response);
+            }
+        }
 
+        public async Task<double> GetActualVoltageAsync()
+        {
+            string response = await scpiProtocol.RequestAsync("MEAS:VOLT");
+
+            if (response.EndsWith(" mV"))
+            {
+                try
+                {
+                    return double.Parse(response.Split(' ')[0]) / 1000;
+                }
+                catch (Exception e)
+                {
+                    throw new UnexpectedResponseException("Could not parse: " + response, e);
+                }
+            }
+            else
+            {
+                throw new UnexpectedResponseException("Unexpected response: " + response);
+            }
+        }
+
+        public async Task<double> GetTemperatureAsync()
+        {
+            string response = await scpiProtocol.RequestAsync("MEAS:TEMP");
+
+            if (response.EndsWith(" C"))
+            {
+                try
+                {
+                    return double.Parse(response.Split(' ')[0]);
+                }
+                catch (Exception e)
+                {
+                    throw new UnexpectedResponseException("Could not parse: " + response, e);
+                }
+            }
+            else
+            {
+                throw new UnexpectedResponseException("Unexpected response: " + response);
+            }
+        }
+
+        public async Task<uint> GetUptimeAsync()
+        {
+            string response = await scpiProtocol.RequestAsync("UPTI");
+
+            if (response.EndsWith(" seconds"))
+            {
+                try
+                {
+                    return uint.Parse(response.Split(' ')[0]);
+                }
+                catch (Exception e)
+                {
+                    throw new UnexpectedResponseException("Could not parse: " + response, e);
+                }
+            }
+            else
+            {
+                throw new UnexpectedResponseException("Unexpected response: " + response);
+            }
+        }
+
+        public async Task<string> GetIdnAsync()
+        {
+            string response = await scpiProtocol.RequestAsync("*IDN");
+
+            // todo: Dissect IDN string
             return response;
         }
+
+        #endregion
+
+        #region Functions: Set system state
+        public async Task SetSetpointCurrentAsync(double current)
+        {
+            if (current < 0)
+            {
+                throw new ArgumentOutOfRangeException("current", current, "Current must be positive.");
+            }
+
+            await scpiProtocol.CommandAsync("CURR " + (int)(current * 1000), "OK");
+        }
+
+        public async Task ResetAsync()
+        {
+            await scpiProtocol.CommandAsync("*RST", "OK");
+        }
+
+        #endregion
     }
 }
